c7e34c8fdacd93cf8509c11d2b563b008b6221c4
ArlessonMoura <arlessonmss@gmail.com>
2024-07-15 03:44:38 -0300

    Finally

--- src/main/java/br/com/oraclechallenge/bookstore/BookstoreApplication.java
+++ src/main/java/br/com/oraclechallenge/bookstore/BookstoreApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
