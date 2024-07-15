fd74d7d433cd9aac92a17ee1e179ffce5bb93f96
ArlessonMoura <arlessonmss@gmail.com>
2024-07-15 01:49:42 -0300

    Interface Repository Author and Book

---
+++ src/main/java/br/com/oraclechallenge/bookstore/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package br.com.oraclechallenge.bookstore.repository;
+
+import br.com.oraclechallenge.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
