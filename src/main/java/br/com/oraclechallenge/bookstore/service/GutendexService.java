3d0be237b584f833efddd7ceb9c201adb6a745d9
ArlessonMoura <arlessonmss@gmail.com>
2024-07-15 19:04:19 -0300

    Redo Rest Template

--- src/main/java/br/com/oraclechallenge/bookstore/service/GutendexService.java
+++ src/main/java/br/com/oraclechallenge/bookstore/service/GutendexService.java
@@ -2,25 +2,31 @@ package br.com.oraclechallenge.bookstore.service;
 
 import br.com.oraclechallenge.bookstore.dto.GutendexResponse;
 import br.com.oraclechallenge.bookstore.model.Book;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class GutendexService {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final String GUTENDEX_API_URL = "https://gutendex.com/books/";
+    private final RestTemplate restTemplate;
+    private final String GUTENDEX_API_URL;
+
+    public GutendexService(RestTemplate restTemplate, @Value("${gutendex.api.url}") String gutendexApiUrl) {
+        this.restTemplate = restTemplate;
+        this.GUTENDEX_API_URL = gutendexApiUrl;
+    }
 
     public List<Book> searchBooks(String query) {
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(GUTENDEX_API_URL)
                 .queryParam("search", query);
 
-        GutendexResponse response = restTemplate.getForObject(builder.toUriString(), GutendexResponse.class);
+        String url = builder.toUriString();
+        GutendexResponse response = restTemplate.getForObject(url, GutendexResponse.class);
 
-        return response != null ? response.getResults() : Collections.emptyList();
+        return response != null ? response.getResults() : List.of();
     }
 }
